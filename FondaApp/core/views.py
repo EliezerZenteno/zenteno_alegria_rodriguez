5484a5942a4d603c5f70e24aa1e2c3ee1f646eb5
Eliezer <elizenteno87@gmail.com>
2025-09-02 11:09:12 -0400

    se renderizo las paginas html y se colocaron en urls.py

--- FondaApp/core/views.py
+++ FondaApp/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def historia(request):
+    return render(request, 'core/historia.html')
+
+def sobre_nosotros(request):
+    return render(request, 'core/sobre_nosotros.html')
+
+def promociones(request):
+    return render(request, 'core/promociones.html')
