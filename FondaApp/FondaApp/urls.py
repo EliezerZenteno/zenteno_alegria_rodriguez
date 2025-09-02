f3f76c5aa8bd0fb77e724c3b507b269728b55a52
Eliezer <elizenteno87@gmail.com>
2025-09-02 11:11:52 -0400

    cambios 4

--- FondaApp/FondaApp/urls.py
+++ FondaApp/FondaApp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('historia/', views.historia, name='historia'),
     path('sobre_nosotros/', views.sobre_nosotros, name='sobre_nosotros'),
     path('promociones/', views.promociones, name='promociones'),
     path('', views.home, name='home'),
--- FondaApp/core/views.py
+++ FondaApp/core/views.py
@@ -3,11 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'core/home.html')
 
-def historia(request):
-    return render(request, 'core/historia.html')
-
 def sobre_nosotros(request):
     return render(request, 'core/sobre_nosotros.html')
 
 def promociones(request):
     return render(request, 'core/promociones.html')
+
